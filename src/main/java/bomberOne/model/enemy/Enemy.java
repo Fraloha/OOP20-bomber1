38cf9f3a8e33d80c73aef1805d70346bc959085d
Weebli <infofrancesco20@gmail.com>
2021-03-31 12:47:57 +0200

    Modified UML and created Enemy interface.

--- src/main/java/bomberOne/model/enemy/Enemy.java
+++ src/main/java/bomberOne/model/enemy/Enemy.java
@@ -9,29 +9,4 @@ import bomberOne.model.gameObjects.GameObject;
  */
 public interface Enemy extends GameObject{
 	
-	/* Methods. */
-	
-	/**
-	 * This method changes the coordinates of the enemy in the 2D space.
-	 * In particular, it set the second coordinate with a value greater than the current.
-	 */
-	public void MoveUp();
-	
-	/**
-	 * This method changes the coordinates of the enemy in the 2D space.
-	 * In particular, it set the second coordinate with a value lesser than the current.
-	 */
-	public void MoveDown();
-	
-	/**
-	 * This method changes the coordinates of the enemy in the 2D space.
-	 * In particular, it set the first coordinate with a value greater than the current.
-	 */
-	public void MoveRight();
-	
-	/**
-	 * This method changes the coordinates of the enemy in the 2D space.
-	 * In particular, it set the first coordinate with a value lesser than the current.
-	 */
-	public void MoveLeft();
 }
