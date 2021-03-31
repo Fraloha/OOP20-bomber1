118327a39c039748f07f9485d26c5a37a30595e3
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2021-03-31 18:43:52 +0200

    Modified P2d

--- src/main/java/bomberOne/model/common/P2d.java
+++ src/main/java/bomberOne/model/common/P2d.java
@@ -2,14 +2,22 @@ package bomberOne.model.common;
 
 public class P2d {
 
-	public double x,y;
+	private double x,y;
 
     public P2d(double x,double y){
         this.x=x;
         this.y=y;
     }
 
-    public P2d update(double time, double velocityX, double velocityY){
+    public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public P2d update(double time, double velocityX, double velocityY){
         return new P2d(x + time * velocityX , y + time * velocityY);
     }
 
