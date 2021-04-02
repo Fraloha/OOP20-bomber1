36c4ff617e073854a3126f56bda12eff8b17128f
LuigiBorriello <luigi.borriello2@studio.unibo.it>
2021-04-02 14:19:17 +0200

    Fix Junit problem

--- src/test/java/bomberOne/model/physics/TestCollision.java
+++ src/test/java/bomberOne/model/physics/TestCollision.java
@@ -1,7 +1,7 @@
 package bomberOne.model.physics;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
