0313cff1f1fb5ab3455c02c5a9e76c5724e4f4b5
Weebli <infofrancesco20@gmail.com>
2021-04-07 15:37:45 +0200

    I do not remember what i have done to this file, but nothing bad.

--- src/main/java/bomberOne/model/enemy/actions/BasicBehavior.java
+++ src/main/java/bomberOne/model/enemy/actions/BasicBehavior.java
@@ -2,6 +2,7 @@ package bomberOne.model.enemy.actions;
 
 import java.util.Random;
 import bomberOne.model.common.P2d;
+import bomberOne.model.gameObjects.AnimatedEntity;
 
 public class BasicBehavior implements Actions{
 	
