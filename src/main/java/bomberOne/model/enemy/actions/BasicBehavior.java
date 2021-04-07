dbb0d0bffdfe9f2d31abb3ad7e99c55aba526b4c
Weebli <infofrancesco20@gmail.com>
2021-04-07 18:15:46 +0200

    Added Enemy object in every implementation of Actions.

--- src/main/java/bomberOne/model/enemy/actions/BasicBehavior.java
+++ src/main/java/bomberOne/model/enemy/actions/BasicBehavior.java
@@ -2,12 +2,18 @@ package bomberOne.model.enemy.actions;
 
 import java.util.Random;
 import bomberOne.model.common.P2d;
-import bomberOne.model.gameObjects.AnimatedEntity;
+import bomberOne.model.enemy.Enemy;
 
 public class BasicBehavior implements Actions{
 	
 	/* Fields. */
 	Random randomGenerator = new Random();
+	Enemy selectedEnemy;
+	
+	/* Constructors. */
+	public BasicBehavior(Enemy newEnemy) {
+		this.selectedEnemy = newEnemy;
+	}
 	
 	/* Methods. */
 	@Override
--- src/main/java/bomberOne/model/enemy/actions/IntermediateBehavior.java
+++ src/main/java/bomberOne/model/enemy/actions/IntermediateBehavior.java
@@ -2,15 +2,18 @@ package bomberOne.model.enemy.actions;
 
 import bomberOne.model.common.P2d;
 import bomberOne.model.observation.EnemyTriggeredObservation;
+import bomberOne.model.enemy.Enemy;
 
 public class IntermediateBehavior implements Actions{
 	
 	/* Fields. */
 	EnemyTriggeredObservation triggeredObs;
+	Enemy selectedEnemy;
 	
 	/* Constructor. */
-	public IntermediateBehavior(P2d playerPosition) {
+	public IntermediateBehavior(P2d playerPosition, Enemy newEnemy) {
 		this.triggeredObs = new EnemyTriggeredObservation(playerPosition);
+		this.selectedEnemy = newEnemy;
 	}
 	
 	/* Methods. */
