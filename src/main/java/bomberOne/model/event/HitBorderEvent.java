532aaef91dcf7fdad0c6851ab01ec01a94f34326
LuigiBorriello <luigi.borriello2@studio.unibo.it>
2021-04-12 11:05:25 +0200

    Reorganize Event Package

--- src/main/java/bomberOne/model/event/HitBorderEvent.java
+++ src/main/java/bomberOne/model/event/HitBorderEvent.java
@@ -1,5 +1,10 @@
 package bomberOne.model.event;
 
+import bomberOne.model.GameModel;
+import bomberOne.model.common.Direction;
+import bomberOne.model.common.P2d;
+import bomberOne.model.enemy.Enemy;
+import bomberOne.model.enemy.EnemyImpl;
 import bomberOne.model.gameObjects.AnimatedEntity;
 import bomberOne.model.gameObjects.GameObject;
 
@@ -10,6 +15,7 @@ import bomberOne.model.gameObjects.GameObject;
  */
 public class HitBorderEvent implements WorldEvent{
 
+	private static final int OBJ_DIMETIONS = 32;
 	private AnimatedEntity entity;
 	private GameObject wall;
 	
@@ -23,6 +29,30 @@ public class HitBorderEvent implements WorldEvent{
 	
 	public GameObject getWall() {
 		return this.wall;
+	}
+
+	/**
+	 * Repositioning of the AniatedEntity that is colliding with the Wall/Box
+	 * @param event
+	 */
+	@Override
+	public void process(GameModel model) {
+		if(this.entity.getDir().equals(Direction.UP)){
+			this.entity.setPosition(new P2d(this.entity.getPosition().getX(), this.wall.getPosition().getY() + OBJ_DIMETIONS));
+		}
+		if(this.entity.getDir().equals(Direction.DOWN)){
+			this.entity.setPosition(new P2d(this.entity.getPosition().getX(), this.wall.getPosition().getY() - OBJ_DIMETIONS));
+		}
+		
+		if(this.entity.getDir().equals(Direction.LEFT)){
+			this.entity.setPosition(new P2d(this.wall.getPosition().getX() + OBJ_DIMETIONS, this.wall.getPosition().getY()));
+		}
+		if(this.entity.getDir().equals(Direction.RIGHT)){
+			this.entity.setPosition(new P2d(this.wall.getPosition().getX() - OBJ_DIMETIONS, this.wall.getPosition().getY()));
+		}
+		if(this.entity.getClass().equals(EnemyImpl.class)) {
+			((Enemy) this.entity).changeDir();
+		}
 	}
 	
 }
--- src/main/java/bomberOne/model/event/WorldEventListenerImpl.java
+++ src/main/java/bomberOne/model/event/WorldEventListenerImpl.java
@@ -2,23 +2,10 @@ package bomberOne.model.event;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import bomberOne.model.GameModel;
-import bomberOne.model.common.Direction;
-import bomberOne.model.common.P2d;
-import bomberOne.model.enemy.Enemy;
-import bomberOne.model.enemy.EnemyImpl;
-import bomberOne.model.gameObjects.BoxImpl;
-import bomberOne.model.gameObjects.Explosion;
-import bomberOne.model.gameObjects.PowerUp;
 
 public class WorldEventListenerImpl implements WorldEventListener {
-
-	private static final int OBJ_DIMETIONS = 32;
-	private static final int BOX_INC_SCORE = 50;
-	private static final int ENEMY_INC_SCORE = 150;
-	private static final int TIMER_INC = 30;
 	
 	private GameModel model;
 	private List<WorldEvent> eventList = new ArrayList<>();
@@ -36,22 +23,7 @@ public class WorldEventListenerImpl implements WorldEventListener {
 	@Override
 	public void processEvents() {
 		this.eventList.stream().forEach(event -> {
-			
-			if (event.getClass().equals(PickPowerUpEvent.class)){
-				this.processPickPowerUpEvent((PickPowerUpEvent) event);
-			}
-			
-			if (event.getClass().equals(HitFireEvent.class)){
-				this.processHitFireEvent((HitFireEvent) event);
-			}
-			
-			if (event.getClass().equals(HitBorderEvent.class)){
-				this.processHitBorderEvent((HitBorderEvent) event);
-			}
-			
-			if (event.getClass().equals(ExplosionEvent.class)){
-				this.processExplosionEvent((ExplosionEvent) event);
-			}
+			event.process(model);
 		});
 		this.eventList.clear();
 	}
@@ -66,130 +38,5 @@ public class WorldEventListenerImpl implements WorldEventListener {
 		return this.model;
 	}
 
-	/*
-	 * When the Bomber picks the PowerUp, this method applies the PowerUp to the Bomber
-	 */
-	private void processPickPowerUpEvent(PickPowerUpEvent event) {
-		if(event.getPowerUp().getType().equals(PowerUp.type.Time)) {
-			this.model.getTime().setTimer(TIMER_INC);
-		}
-		else {
-			this.model.getWorld().getBomber().applyPowerUp(event.getPowerUp().getType());			
-		}
-		event.getPowerUp().hitted();
-	}
-	
-	/**
-	 * When an GameObject is Colliding with the fire, it calls "hitted()" on the Object
-	 * @param event
-	 */
-	private void processHitFireEvent(HitFireEvent event) {
-		event.getEntity().hitted();
-		if(event.getEntity().getClass().equals(BoxImpl.class)) {
-			this.model.incScore(BOX_INC_SCORE);
-		}
-		if(event.getEntity().getClass().equals(EnemyImpl.class)) {
-			this.model.incScore(ENEMY_INC_SCORE);
-		}
-	}
-	
-	/**
-	 * Repositioning of the AniatedEntity that is colliding with the Wall/Box
-	 * @param event
-	 */
-	private void processHitBorderEvent(HitBorderEvent event) {
-		if(event.getEntity().getDir().equals(Direction.UP)){
-			event.getEntity().setPosition(new P2d(event.getEntity().getPosition().getX(), event.getWall().getPosition().getY() + OBJ_DIMETIONS));
-		}
-		if(event.getEntity().getDir().equals(Direction.DOWN)){
-			event.getEntity().setPosition(new P2d(event.getEntity().getPosition().getX(), event.getWall().getPosition().getY() - OBJ_DIMETIONS));
-		}
-		
-		if(event.getEntity().getDir().equals(Direction.LEFT)){
-			event.getEntity().setPosition(new P2d(event.getWall().getPosition().getX() + OBJ_DIMETIONS, event.getEntity().getPosition().getY()));
-		}
-		if(event.getEntity().getDir().equals(Direction.RIGHT)){
-			event.getEntity().setPosition(new P2d(event.getWall().getPosition().getX() - OBJ_DIMETIONS, event.getEntity().getPosition().getY()));
-		}
-		if(event.getEntity().getClass().equals(EnemyImpl.class)) {
-			((Enemy) event.getEntity()).changeDir();
-		}
-	}
-	
-	/**
-	 * Handle & Resize the explosion and Spawn the Fire on the Map
-	 * @param event
-	 */
-	private void processExplosionEvent(ExplosionEvent event) {
-		Explosion exp = event.getExplosion();
-		this.model.getWorld().getGameObjectCollection().spawn(this.model.getWorld().getGameObjectFactory().createFire(exp.getCenter()));
-		List<P2d> wallsListPos = new ArrayList<>();
-		List<P2d> boxListPos = new ArrayList<>();
-		boxListPos.addAll(this.model.getWorld().getGameObjectCollection().getBoxList().stream().map(e -> e.getPosition()).collect(Collectors.toList()));
-		wallsListPos.addAll(this.model.getWorld().getGameObjectCollection().getHardWallList().stream().map(e -> e.getPosition()).collect(Collectors.toList()));
-		
-		//Left Direction
-		for(int i = 1; i < exp.getFirePower(); i++) {
-			P2d newPos = new P2d(exp.getCenter().getX() - OBJ_DIMETIONS, exp.getCenter().getY());
-		//The fire cannot be spawned over the Walls, but it can be spawner over the other Objects
-			if(!wallsListPos.contains(newPos)) {
-				this.model.getWorld().getGameObjectCollection().spawn(this.model.getWorld().getGameObjectFactory().createFire(newPos));				
-			}
-			else {
-				break;
-			}
-			if(boxListPos.contains(newPos) && !exp.getPierce()) {
-				break;
-			}
-		}
-		
-		//Right Direction
-		for(int i = 1; i < exp.getFirePower(); i++) {
-			P2d newPos = new P2d(exp.getCenter().getX() + OBJ_DIMETIONS, exp.getCenter().getY());
-		//The fire cannot be spawned over the Walls, but it can be spawner over the other Objects
-			if(!wallsListPos.contains(newPos)) {
-				this.model.getWorld().getGameObjectCollection().spawn(this.model.getWorld().getGameObjectFactory().createFire(newPos));				
-			}
-			else {
-				break;
-			}
-			if(boxListPos.contains(newPos) || !exp.getPierce()) {
-				break;
-			}
-		}
-		
-		//Up Directions
-		for(int i = 1; i < exp.getFirePower(); i++) {
-			P2d newPos = new P2d(exp.getCenter().getX(), exp.getCenter().getY() - OBJ_DIMETIONS);
-		//The fire cannot be spawned over the Walls, but it can be spawner over the other Objects
-			if(!wallsListPos.contains(newPos)) {
-				this.model.getWorld().getGameObjectCollection().spawn(this.model.getWorld().getGameObjectFactory().createFire(newPos));				
-			}
-			else {
-				break;
-			}
-			if(boxListPos.contains(newPos) || !exp.getPierce()) {
-				break;
-			}
-		}
-		
-		//Down Directions
-		for(int i = 1; i < exp.getFirePower(); i++) {
-			P2d newPos = new P2d(exp.getCenter().getX(), exp.getCenter().getY() + OBJ_DIMETIONS);
-		//The fire cannot be spawned over the Walls, but it can be spawner over the other Objects	
-			if(!wallsListPos.contains(newPos)) {
-				this.model.getWorld().getGameObjectCollection().spawn(this.model.getWorld().getGameObjectFactory().createFire(newPos));				
-			}
-			else {
-				break;
-			}
-			if(boxListPos.contains(newPos) && !exp.getPierce()) {
-				break;
-			}
-		}
-		
-		this.model.getWorld().getBomber().incAmmo(1);
-		
-	}
 }
 
