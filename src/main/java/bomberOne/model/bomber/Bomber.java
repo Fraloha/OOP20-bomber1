1e79e78dbaae39fc87ff9ce979fab1980ae9dd32
LuigiBorriello <luigi.borriello2@studio.unibo.it>
2021-04-17 11:25:26 +0200

    Refactor

--- src/main/java/bomberOne/model/bomber/Bomber.java
+++ src/main/java/bomberOne/model/bomber/Bomber.java
@@ -6,83 +6,93 @@ import bomberOne.model.common.Direction;
 import bomberOne.model.gameObjects.AnimatedEntity;
 import bomberOne.model.gameObjects.Bomb;
 import bomberOne.model.gameObjects.PowerUp;
-/**This is the interface of the Bomber 
- * @author Gustavo Mazzanti
+
+/**
+ * This is the interface of the Bomber.
+ * 
  *
  */
 public interface Bomber extends AnimatedEntity {
-	/**
-	 * Constants to initialize the Bomber
-	 */
-	static final int FIRE_POWER = 1;
-	static final double SPEED = 100;
-	static final int AMMO = 1;
-	static final int SPRITES = 1;
-	static final Direction DIR = Direction.DOWN;
-	
-	/**
-	 * Method for the respawn of the Bomber
-	 * who initialize all the parameter
-	 */
-	void respawn();
+    /**
+     * Constants to initialize the Bomber.
+     */
+    int FIRE_POWER = 1;
+    double SPEED = 100;
+    int AMMO = 1;
+    int SPRITES = 1;
+    Direction DIR = Direction.DOWN;
 
-	/**
-	 * Method for increments the lifes of
-	 * @param lifes
-	 */
-	void addLifes(int lifes);
+    /**
+     * Method for the respawn of the Bomber who initialize all the parameter.
+     */
+    void respawn();
 
-	/**
-	 * Method for restore the usedAmmo to 0
-	 */
-	void restoreAmmo();
+    /**
+     * Method for increments the lifes of.
+     * 
+     * @param lifes
+     */
+    void addLifes(int lifes);
 
-	/**
-	 * Method that generates new Bomb
-	 * @return Bomb
-	 */
-	Optional<Bomb> plantBomb();
+    /**
+     * Method for restore the usedAmmo to 0
+     */
+    void restoreAmmo();
 
-	/**
-	 * Method that applies the 
-	 * @param PowerUp to the Bomber
-	 */
-	void applyPowerUp(PowerUp.Type typeOfPowerUp);
+    /**
+     * Method that generates new Bomb
+     * 
+     * @return Bomb
+     */
+    Optional<Bomb> plantBomb();
 
-	/**
-	 * Method that
-	 * @return number of the Life
-	 */
-	int getLifes();
-	
-	/**
-	 * Method that
-	 * @return number of maxAmmo
-	 */
-	int getAmmo();
-	
-	/**
-	 * Method that
-	 * @return dimension of the bomb's explosion
-	 */
-	int getFirePower();
-	
-	/**
-	 * Method that
-	 * @return Direction current
-	 */
-	Direction getDirection();
+    /**
+     * Method that applies the
+     * 
+     * @param PowerUp to the Bomber
+     */
+    void applyPowerUp(PowerUp.Type typeOfPowerUp);
 
-	/**
-	 * Method that
-	 * @return true if Bomber is Pierced (PowerUp)
-	 */
-	boolean isPierced();
-	
-	/** Method for attaching 
-	 * @param PowerUpHandler
-	 */
-	void setUpHandler(PowerUpHandler activator);
+    /**
+     * Method that
+     * 
+     * @return number of the Life
+     */
+    int getLifes();
 
+    /**
+     * Method that
+     * 
+     * @return number of maxAmmo
+     */
+    int getAmmo();
+
+    /**
+     * Method that
+     * 
+     * @return dimension of the bomb's explosion
+     */
+    int getFirePower();
+
+    /**
+     * Method that
+     * 
+     * @return Direction current
+     */
+    Direction getDirection();
+
+    /**
+     * Method that
+     * 
+     * @return true if Bomber is Pierced (PowerUp)
+     */
+    boolean isPierced();
+
+    /**
+     * Method for attaching
+     * 
+     * @param PowerUpHandler
+     */
+    void setUpHandler(PowerUpHandler activator);
 
 }
--- src/main/java/bomberOne/model/bomber/BomberImpl.java
+++ src/main/java/bomberOne/model/bomber/BomberImpl.java
@@ -11,228 +11,230 @@ import bomberOne.model.gameObjects.Bomb;
 import bomberOne.model.gameObjects.BombImpl;
 import bomberOne.model.gameObjects.PowerUp.Type;
 import bomberOne.tools.ResourcesLoader;
-import bomberOne.tools.img.AnimatedObjectsSprites;
-import bomberOne.tools.img.ObjectsImages;
 
-public class BomberImpl extends AnimatedEntityImpl implements Bomber {
-	public static final double SPEED_INC = 2;
-	public static final int AMMO_INC = 1;
-	public static final int FIRE_POWER_INC = 2;
-	private final P2d startPosition;
-	private int firePower;
-	private boolean pierce;
-	private int maxAmmo;
-	private int usedAmmo;
-	private PowerUpHandler activator;
-	private int fpAgg = 0;
-	//Images and animations
-	private int spriteIndex; //0 UP, 1 DOWN, 2 LEFT, 3 RIGHT, 4 DEATH
-	private int animationIndex;
-	private BufferedImage[][] animations;
-	private boolean isChangedDir = false;
+public final class BomberImpl extends AnimatedEntityImpl implements Bomber {
 
-	public BomberImpl(P2d pos, BufferedImage[][] img, int lifes) {
-		super(pos, img[0][1], lifes);
-		this.setSpeed(SPEED);
-		this.setDir(DIR);
-		this.animations = img;
-		this.startPosition = pos;
-		this.firePower  = FIRE_POWER;
-		this.pierce = false;
-		this.maxAmmo = AMMO;
-		this.usedAmmo = 0;
-		this.spriteIndex = SPRITES;
-		this.animationIndex = 0;
-		ResourcesLoader.start();
-	}
-	
-	@Override
-	public void respawn() {
-		this.setSpeed(SPEED);
-		this.setDir(DIR);
-		this.setAlive(true);
-		this.setPosition(startPosition);
-		this.firePower  = FIRE_POWER;
-		this.pierce = false;
-		this.maxAmmo = AMMO;
-		this.usedAmmo = 0;
-		this.spriteIndex = SPRITES;
-		this.animationIndex = 0;
-		this.fpAgg = 0;
-	}
-	
-	@Override
-	public void addLifes(int lifes) {
-		this.setLifes(this.getLifes() + lifes);
-	}
-	
-	@Override
-	public void restoreAmmo() {
-		if(this.usedAmmo>0) {
-			this.usedAmmo--;
-		}
-	}
-	
-	@Override
-	public Optional<Bomb> plantBomb() {
-		if(this.maxAmmo>this.usedAmmo) {
-			usedAmmo++;
-			return Optional.of((BombImpl) new GameObjectFactoryImpl().createBomb(new P2d(this.getPosition().getX(), this.getPosition().getY()), this.firePower, this.pierce));
-		}
-		return Optional.empty();
-	}
-	
-	@Override
-	public void applyPowerUp(Type typeOfPowerUp){
-		switch (typeOfPowerUp) {
-			case FirePower:
-				this.activator.applyFirePower(FIRE_POWER_INC);
-				break;
-			case Ammo:
-				this.activator.applyMultiAmmo(AMMO_INC);
-				break;
-			case Pierce:
-				this.activator.applyPierce();
-				break;
-			case Speed:
-				this.activator.applySpeed(SPEED_INC);
-				break;
-			case Time:
-				break;
-			default:
-				break;
-		}
-	}
-	
-	
-	@Override
-	public BufferedImage getImage() {
-		return this.animations[this.spriteIndex][this.animationIndex%4];
-	}
-	
-	@Override
-	public int getLifes() {
-		return this.getLifes();
-	}
-	
-	@Override
-	public int getAmmo() {
-		return this.maxAmmo - this.usedAmmo;
-	}
-	
-	@Override
-	public int getFirePower() {
-		return this.firePower;
-	}
-	
-	@Override
-	public Direction getDirection(){
-		return this.getDir();
-	}
-	
-	@Override
-	public boolean isPierced() {
-		return this.pierce;
-	}
-	
-	@Override
-	public void setUpHandler(PowerUpHandler activator) {
-		this.activator = activator;
-	}
-	
-	@Override
-	public void moveUp() {
-		if(this.getDirection() != Direction.UP) {
-			this.isChangedDir = true;
-			this.setDir(Direction.UP);
-		}
-		super.moveUp();
-	}
 
-	@Override
-	public void moveDown() {
-		if(this.getDirection() != Direction.DOWN) {
-			this.isChangedDir = true;
-			this.setDir(Direction.DOWN);
-		}
-		super.moveDown();
-	}
+    public static final double SPEED_INC = 2;
+    public static final int AMMO_INC = 1;
+    public static final int FIRE_POWER_INC = 2;
 
-	@Override
-	public void moveLeft() {
-		if(this.getDirection() != Direction.LEFT) {
-			this.isChangedDir = true;
-			this.setDir(Direction.LEFT);
-		}
-		super.moveLeft();
-	}
+    private final P2d startPosition;
+    private int firePower;
+    private boolean pierce;
+    private int maxAmmo;
+    private int usedAmmo;
+    private PowerUpHandler activator;
+    private int fpAgg = 0;
+    // Images and animations
+    private int spriteIndex; // 0 UP, 1 DOWN, 2 LEFT, 3 RIGHT, 4 DEATH
+    private int animationIndex;
+    private BufferedImage[][] animations;
+    private boolean isChangedDir = false;
 
-	@Override
-	public void moveRight() {
-		if(this.getDirection() != Direction.RIGHT) {
-			this.isChangedDir = true;
-			this.setDir(Direction.RIGHT);
-		}
-		super.moveRight();
-	}
-	
-	@Override
-	public void hitted() {
-		this.setLifes(this.getLifes() - 1);
-		this.setAlive(false);
-	}
+    public BomberImpl(final P2d pos, final BufferedImage img[][], final int lifes) {
+        super(pos, img[0][1], lifes);
+        this.setSpeed(SPEED);
+        this.setDir(DIR);
+        this.animations = img;
+        this.startPosition = pos;
+        this.firePower = FIRE_POWER;
+        this.pierce = false;
+        this.maxAmmo = AMMO;
+        this.usedAmmo = 0;
+        this.spriteIndex = SPRITES;
+        this.animationIndex = 0;
+        ResourcesLoader.start();
+    }
 
-	@Override
-	public void update(int elapsed) {
-		if(this.isAlive()) {
-			if(this.isChangedDir) {
-				switch (this.getDir()) {
-				case UP:
-					this.spriteIndex = 0;
-					this.animationIndex = 0;
-					break;
-				case DOWN:
-					this.spriteIndex  = 1;
-					this.animationIndex = 0;
-					break;
-				case LEFT:
-					this.spriteIndex = 2;
-					this.animationIndex = 0;
-					break;
-				case RIGHT:
-					this.spriteIndex = 3;
-					this.animationIndex = 0;
-					break;
-				}
-				this.isChangedDir = false;
-			}
-		} else {
-			this.spriteIndex = 4;
-			this.animationIndex = 0;
-		}
-		if(++this.fpAgg == 4) {
-			this.fpAgg = 0;
-			this.animationIndex++;
-		}
-		if(this.spriteIndex == 4 && this.animationIndex%4 == 3) {
-			this.respawn();
-		}
-		super.update(elapsed);
-	}
 
-	protected void incSpeed(double increment) {
-		this.setSpeed(this.getSpeed() + increment);
-	}
-	
-	public void incAmmo(int increment) {
-		this.maxAmmo += increment;
-	}
-	
-	protected void incFirePower(int increment) {
-		this.firePower += increment;
-	}
-	
-	protected void activatePierce() {
-		this.pierce = true;
-	}
+    @Override
+    public void respawn() {
+        this.setSpeed(SPEED);
+        this.setDir(DIR);
+        this.setAlive(true);
+        this.setPosition(startPosition);
+        this.firePower = FIRE_POWER;
+        this.pierce = false;
+        this.maxAmmo = AMMO;
+        this.usedAmmo = 0;
+        this.spriteIndex = SPRITES;
+        this.animationIndex = 0;
+        this.fpAgg = 0;
+    }
+
+    @Override
+    public void addLifes(int lifes) {
+        this.setLifes(this.getLifes() + lifes);
+    }
+
+    @Override
+    public void restoreAmmo() {
+        if (this.usedAmmo > 0) {
+            this.usedAmmo--;
+        }
+    }
+
+    @Override
+    public Optional<Bomb> plantBomb() {
+        if (this.maxAmmo > this.usedAmmo) {
+            usedAmmo++;
+            return Optional.of((BombImpl) new GameObjectFactoryImpl().createBomb(
+                    new P2d(this.getPosition().getX(), this.getPosition().getY()), this.firePower, this.pierce));
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public void applyPowerUp(Type typeOfPowerUp) {
+        switch (typeOfPowerUp) {
+        case FirePower:
+            this.activator.applyFirePower(FIRE_POWER_INC);
+            break;
+        case Ammo:
+            this.activator.applyMultiAmmo(AMMO_INC);
+            break;
+        case Pierce:
+            this.activator.applyPierce();
+            break;
+        case Speed:
+            this.activator.applySpeed(SPEED_INC);
+            break;
+        case Time:
+            break;
+        default:
+            break;
+        }
+    }
+
+    @Override
+    public BufferedImage getImage() {
+        return this.animations[this.spriteIndex][this.animationIndex % 4];
+    }
+
+    @Override
+    public int getLifes() {
+        return this.getLifes();
+    }
+
+    @Override
+    public int getAmmo() {
+        return this.maxAmmo - this.usedAmmo;
+    }
+
+    @Override
+    public int getFirePower() {
+        return this.firePower;
+    }
+
+    @Override
+    public Direction getDirection() {
+        return this.getDir();
+    }
+
+    @Override
+    public boolean isPierced() {
+        return this.pierce;
+    }
+
+    @Override
+    public void setUpHandler(PowerUpHandler activator) {
+        this.activator = activator;
+    }
+
+    @Override
+    public void moveUp() {
+        if (this.getDirection() != Direction.UP) {
+            this.isChangedDir = true;
+            this.setDir(Direction.UP);
+        }
+        super.moveUp();
+    }
+
+    @Override
+    public void moveDown() {
+        if (this.getDirection() != Direction.DOWN) {
+            this.isChangedDir = true;
+            this.setDir(Direction.DOWN);
+        }
+        super.moveDown();
+    }
+
+    @Override
+    public void moveLeft() {
+        if (this.getDirection() != Direction.LEFT) {
+            this.isChangedDir = true;
+            this.setDir(Direction.LEFT);
+        }
+        super.moveLeft();
+    }
+
+    @Override
+    public void moveRight() {
+        if (this.getDirection() != Direction.RIGHT) {
+            this.isChangedDir = true;
+            this.setDir(Direction.RIGHT);
+        }
+        super.moveRight();
+    }
+
+    @Override
+    public void hitted() {
+        this.setLifes(this.getLifes() - 1);
+        this.setAlive(false);
+    }
+
+    @Override
+    public void update(int elapsed) {
+        if (this.isAlive()) {
+            if (this.isChangedDir) {
+                switch (this.getDir()) {
+                case UP:
+                    this.spriteIndex = 0;
+                    this.animationIndex = 0;
+                    break;
+                case DOWN:
+                    this.spriteIndex = 1;
+                    this.animationIndex = 0;
+                    break;
+                case LEFT:
+                    this.spriteIndex = 2;
+                    this.animationIndex = 0;
+                    break;
+                case RIGHT:
+                    this.spriteIndex = 3;
+                    this.animationIndex = 0;
+                    break;
+                }
+                this.isChangedDir = false;
+            }
+        } else {
+            this.spriteIndex = 4;
+            this.animationIndex = 0;
+        }
+        if (++this.fpAgg == 4) {
+            this.fpAgg = 0;
+            this.animationIndex++;
+        }
+        if (this.spriteIndex == 4 && this.animationIndex % 4 == 3) {
+            this.respawn();
+        }
+        super.update(elapsed);
+    }
+
+    protected void incSpeed(double increment) {
+        this.setSpeed(this.getSpeed() + increment);
+    }
+
+    public void incAmmo(int increment) {
+        this.maxAmmo += increment;
+    }
+
+    protected void incFirePower(int increment) {
+        this.firePower += increment;
+    }
+
+    protected void activatePierce() {
+        this.pierce = true;
+    }
 }
