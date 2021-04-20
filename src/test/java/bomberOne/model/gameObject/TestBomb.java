7666e2cfd49245eddba1fd30da95661861cf0184
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2021-04-20 18:24:00 +0200

    Solved checkstyle on tests

--- src/test/java/bomberOne/model/gameObject/TestBomb.java
+++ src/test/java/bomberOne/model/gameObject/TestBomb.java
@@ -12,38 +12,40 @@ import bomberOne.model.common.P2d;
 import bomberOne.model.factory.GameObjectFactoryImpl;
 import bomberOne.model.gameObjects.BombImpl;
 import bomberOne.model.gameObjects.Explosion;
-import bomberOne.tools.img.ObjectsImages;
 
 /**
  * Tester for Bomb.
  *
  */
 public class TestBomb {
-	BombImpl bomb;
-	Optional<Explosion> explosion;
-	
-	@BeforeEach
-	public void init() {
-		this.bomb = (BombImpl) new GameObjectFactoryImpl().createBomb(new P2d(0, 0), 1, false);
-	}
-	
-	@Test
-	public void testOptionalExplosion() {
-		this.explosion = Optional.empty();
-		assertTrue(this.bomb.getExplosion().equals(this.explosion));
-		this.explosion = Optional.of(this.bomb.explode());
-		assertTrue(this.bomb.getExplosion().equals(this.explosion));
-	}
-	
-	@Test
-	public void testUpdate() {
-		for(int c = 0; c<270; c++) {
-			this.bomb.update(0);
-		}
-		assertTrue(this.bomb.isAlive());
-		assertTrue(this.bomb.getExplosion().equals(Optional.empty()));
-		this.bomb.update(0);
-		assertFalse(this.bomb.isAlive());
-		assertFalse(this.bomb.getExplosion().equals(Optional.empty()));
-	}
+    private BombImpl bomb;
+    private Optional<Explosion> explosion;
+
+    /**
+     * 
+     */
+    @BeforeEach
+    public void init() {
+        this.bomb = (BombImpl) new GameObjectFactoryImpl().createBomb(new P2d(0, 0), 1, false);
+    }
+
+    @Test
+    public void testOptionalExplosion() {
+        this.explosion = Optional.empty();
+        assertTrue(this.bomb.getExplosion().equals(this.explosion));
+        this.explosion = Optional.of(this.bomb.explode());
+        assertTrue(this.bomb.getExplosion().equals(this.explosion));
+    }
+
+    @Test
+    public void testUpdate() {
+        for (int c = 0; c < bomb.TIME_TO_EXPLODE; c++) {
+            this.bomb.update(0);
+        }
+        assertTrue(this.bomb.isAlive());
+        assertTrue(this.bomb.getExplosion().equals(Optional.empty()));
+        this.bomb.update(0);
+        assertFalse(this.bomb.isAlive());
+        assertFalse(this.bomb.getExplosion().equals(Optional.empty()));
+    }
 }
