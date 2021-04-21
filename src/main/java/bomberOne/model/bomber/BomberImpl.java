ba6e5ff34782f4895dbe57777f7ceda805fc4bc0
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2021-04-21 13:00:23 +0200

    Solving problem movement Bomber

--- src/main/java/bomberOne/model/bomber/BomberImpl.java
+++ src/main/java/bomberOne/model/bomber/BomberImpl.java
@@ -203,11 +203,13 @@ public final class BomberImpl extends AnimatedEntityImpl implements Bomber {
      */
     @Override
     public void moveUp() {
+        this.setStatic(false);
         if (this.getDirection() != Direction.UP) {
             this.isChangedDir = true;
             this.setDir(Direction.UP);
         }
         super.moveUp();
+        this.setStatic(true);
     }
 
     /**
@@ -215,11 +217,13 @@ public final class BomberImpl extends AnimatedEntityImpl implements Bomber {
      */
     @Override
     public void moveDown() {
+        this.setStatic(false);
         if (this.getDirection() != Direction.DOWN) {
             this.isChangedDir = true;
             this.setDir(Direction.DOWN);
         }
         super.moveDown();
+        this.setStatic(true);
     }
 
     /**
@@ -227,11 +231,13 @@ public final class BomberImpl extends AnimatedEntityImpl implements Bomber {
      */
     @Override
     public void moveLeft() {
+        this.setStatic(false);
         if (this.getDirection() != Direction.LEFT) {
             this.isChangedDir = true;
             this.setDir(Direction.LEFT);
         }
         super.moveLeft();
+        this.setStatic(true);
     }
 
     /**
@@ -239,11 +245,13 @@ public final class BomberImpl extends AnimatedEntityImpl implements Bomber {
      */
     @Override
     public void moveRight() {
+        this.setStatic(false);
         if (this.getDirection() != Direction.RIGHT) {
             this.isChangedDir = true;
             this.setDir(Direction.RIGHT);
         }
         super.moveRight();
+        this.setStatic(true);
     }
 
     /**
@@ -288,7 +296,7 @@ public final class BomberImpl extends AnimatedEntityImpl implements Bomber {
             this.spriteIndex = 4;
             this.animationIndex = 0;
         }
-        if (++this.fpAgg == 4) {
+        if (!this.isStatic() && ++this.fpAgg == 4) {
             this.fpAgg = 0;
             this.animationIndex++;
         }
