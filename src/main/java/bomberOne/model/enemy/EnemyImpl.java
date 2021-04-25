87b6d7666a83a7f2ba16bb02befebc43260e80a6
Weebli <infofrancesco20@gmail.com>
2021-04-26 00:22:37 +0200

    Deleted changePath() method

--- src/main/java/bomberOne/model/enemy/EnemyImpl.java
+++ src/main/java/bomberOne/model/enemy/EnemyImpl.java
@@ -1,6 +1,5 @@
 package bomberOne.model.enemy;
 
-import bomberOne.model.common.Direction;
 import bomberOne.model.common.P2d;
 import bomberOne.model.gameObjects.AnimatedEntityImpl;
 import java.awt.image.BufferedImage;
@@ -8,8 +7,6 @@ import bomberOne.model.enemy.actions.Actions;
 import bomberOne.model.enemy.actions.IntermediateBehavior;
 import bomberOne.model.enemy.actions.BasicBehavior;
 import bomberOne.model.user.Difficulty;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public final class EnemyImpl extends AnimatedEntityImpl implements Enemy {
 
@@ -18,9 +15,6 @@ public final class EnemyImpl extends AnimatedEntityImpl implements Enemy {
         private static final int FRAME_PER_SECOND = 60;
         private Actions behavior;
         private int frameCounter;
-        private Queue<P2d> badPath;
-        private Queue<P2d> normalPath;
-        private Queue<Direction> nextDirections;
         
         /* Constructors. */
         public EnemyImpl(final P2d position, final BufferedImage [][] img, final int lifes, Difficulty mode) {
@@ -28,11 +22,7 @@ public final class EnemyImpl extends AnimatedEntityImpl implements Enemy {
             
             //Setting the number of frames that the enemy has to wait before start moving.
             this.frameCounter = SECONDS_TO_WAIT * FRAME_PER_SECOND;
-            
-            this.badPath = new LinkedList<P2d>();
-            this.normalPath = new LinkedList<P2d>();
-            this.nextDirections = new LinkedList<Direction>();
-            
+
             //Creating the enemy behavior on the basis of the mode chosen by the user.
             if (mode.equals(Difficulty.STANDARD)) {
                 this.behavior = new BasicBehavior(this);
@@ -64,16 +54,5 @@ public final class EnemyImpl extends AnimatedEntityImpl implements Enemy {
                 //Executing the behavior.
                 this.behavior.doActions();
             }
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public void changePath() {
-            //Setting the bad path to not walk through the path that makes the enemy
-            //collide.
-            this.badPath = this.normalPath;
-            this.normalPath.clear();
         }
 }
