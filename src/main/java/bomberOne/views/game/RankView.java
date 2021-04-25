da6c6164f7374e27b5cf76beddd4abeeb426ee20
Weebli <infofrancesco20@gmail.com>
2021-04-25 16:53:38 +0200

    Created RankViewImages directory and added the rank view title image.

---
+++ src/main/java/bomberOne/views/game/RankView.java
@@ -0,0 +1,16 @@
+package bomberOne.views.game;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RankView extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
