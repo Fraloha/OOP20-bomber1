425f3e4b5b95a7af5f05a9d6aede7d226221ad03
Weebli <infofrancesco20@gmail.com>
2021-04-27 00:12:28 +0200

    Deleted Observation objects.

--- src/main/java/bomberOne/model/observation/SimpleObservation.java
+++
@@ -1,9 +0,0 @@
-package bomberOne.model.observation;
-
-import bomberOne.model.common.P2d;
-
-public interface SimpleObservation extends Observation {
-
-        /* Methods. */
-        P2d calculateDistance(P2d point);
-}
