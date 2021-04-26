3e30082e6e96233d7f4a71933e3abefaf0a730c0
Weebli <infofrancesco20@gmail.com>
2021-04-26 23:02:56 +0200

    Implemented nextMove() method in BasicBehavior class.

--- src/main/java/bomberOne/model/enemy/actions/BasicBehavior.java
+++ src/main/java/bomberOne/model/enemy/actions/BasicBehavior.java
@@ -4,63 +4,72 @@ import java.util.Random;
 import bomberOne.model.common.Direction;
 import bomberOne.model.enemy.EnemyImpl;
 
-public class BasicBehavior implements Actions{
+public final class BasicBehavior implements Actions{
 	
 	/* Fields. */
 	private Random randomGenerator = new Random();
 	private EnemyImpl selectedEnemy;
 	
 	/* Constructors. */
-	public BasicBehavior(EnemyImpl newEnemy) {
+	public BasicBehavior(final EnemyImpl newEnemy) {
 		this.selectedEnemy = newEnemy;
 	}
 	
 	/* Methods. */
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void doActions() {
-	    //Checking if the enemy changed direction.
-	    if (this.selectedEnemy.getDir() != this.selectedEnemy.getPreviousDirection()) {
-	        this.selectedEnemy.setPreviousDirection(this.selectedEnemy.getDir());
+	    int newMovement = this.randomGenerator.nextInt(4);
+	    if (newMovement != this.selectedEnemy.getDir().ordinal()) {
+	        this.selectedEnemy.setDir(Direction.values()[newMovement]);
 	        
-	        //Moving the enemy on the basis of the generated random value.
-	        switch(this.randomGenerator.nextInt(4)) {
-	        
-	        case 0:
-	            this.selectedEnemy.setSpriteIndex(3);
-	            this.selectedEnemy.setAnimationIndex(0);
-	            this.selectedEnemy.setDir(Direction.UP);
-	            this.selectedEnemy.moveUp();
-	            break;
-	            
-	        case 1:
-	            this.selectedEnemy.setSpriteIndex(2);
-	            this.selectedEnemy.setAnimationIndex(0);
-	            this.selectedEnemy.setDir(Direction.RIGHT);
-	            this.selectedEnemy.moveRight();
-	            break;
-	            
-	        case 2:
-	            this.selectedEnemy.setSpriteIndex(1);
-	            this.selectedEnemy.setAnimationIndex(0);
-	            this.selectedEnemy.setDir(Direction.LEFT);
-	            this.selectedEnemy.moveLeft();
-	            break;
-	            
-	        case 3:
+	        //Setting the sprite.
+	        switch(this.selectedEnemy.getDir()) {
+	        case UP:
 	            this.selectedEnemy.setSpriteIndex(0);
-	            this.selectedEnemy.setAnimationIndex(0);
-	            this.selectedEnemy.setDir(Direction.DOWN);
-	            this.selectedEnemy.moveDown();
+	            break;
+	            
+	        case LEFT:
+	            this.selectedEnemy.setSpriteIndex(1);
+	            break;
+	            
+	        case RIGHT:
+	            this.selectedEnemy.setSpriteIndex(2);
+	            break;
+	            
+	        case DOWN:
+	            this.selectedEnemy.setSpriteIndex(3);
 	            break;
 	        }
+	        
+	        this.selectedEnemy.setAnimationIndex(0);
+	        
 	    } else {
-	        //Animating the enemy.
-	        if (this.selectedEnemy.getFrameCounterAnimation() == 10) {
+	        if (this.selectedEnemy.getFrameCounterAnimation() == 5) {
 	            this.selectedEnemy.setFrameCounterAnimation(0);
-	            this.selectedEnemy.setAnimationIndex((this.selectedEnemy.getAnimationIndex() + 1) % 4);
 	        } else {
 	            this.selectedEnemy.setFrameCounterAnimation(this.selectedEnemy.getFrameCounterAnimation() + 1);
 	        }
+	    }
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void nextMove() {
+	    //Moving the enemy on the basis of the direction.
+	    if (this.selectedEnemy.getDir() == Direction.UP) {
+	        this.selectedEnemy.moveUp();
+	    } else if (this.selectedEnemy.getDir() == Direction.DOWN) {
+	        this.selectedEnemy.moveDown();
+	    } else if (this.selectedEnemy.getDir() == Direction.RIGHT) {
+	        this.selectedEnemy.moveRight();
+	    } else {
+	        this.selectedEnemy.moveLeft();
 	    }
 	}
 }
