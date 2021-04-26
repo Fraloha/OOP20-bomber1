283628f25c4935eb97146de46901facb37f868ef
Weebli <infofrancesco20@gmail.com>
2021-04-26 15:27:58 +0200

    Updated BasicBehavior.java

--- src/main/java/bomberOne/model/enemy/EnemyImpl.java
+++ src/main/java/bomberOne/model/enemy/EnemyImpl.java
@@ -1,5 +1,6 @@
 package bomberOne.model.enemy;
 
+import bomberOne.model.common.Direction;
 import bomberOne.model.common.P2d;
 import bomberOne.model.gameObjects.AnimatedEntityImpl;
 import java.awt.image.BufferedImage;
@@ -16,6 +17,7 @@ public final class EnemyImpl extends AnimatedEntityImpl implements Enemy {
         private static final int LOW_SPEED = 200;
         private static final int HIGH_SPEED = 300;
         private Actions behavior;
+        private Direction previousDirection;
         private int frameCounter;
         
         /* Constructors. */
@@ -24,7 +26,9 @@ public final class EnemyImpl extends AnimatedEntityImpl implements Enemy {
             
             //Setting the number of frames that the enemy has to wait before start moving.
             this.frameCounter = SECONDS_TO_WAIT * FRAME_PER_SECOND;
-
+            
+            this.previousDirection = Direction.DOWN;
+            
             //Creating the enemy behavior on the basis of the mode chosen by the user.
             if (mode.equals(Difficulty.STANDARD)) {
                 this.behavior = new BasicBehavior(this);
@@ -50,5 +54,13 @@ public final class EnemyImpl extends AnimatedEntityImpl implements Enemy {
                 this.behavior.doActions();
                 super.update(elapsed);
             }
+        }
+        
+        public Direction getPreviousDirection() {
+            return this.previousDirection;
+        }
+        
+        public void setPreviousDirection(Direction newDirection) {
+            this.previousDirection = newDirection;
         }
 }
