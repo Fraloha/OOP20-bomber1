f4d0c47089b8272b6572d336ff58a10112eeb759
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2021-04-28 16:59:29 +0200

    Created HomeView FXML

---
+++ src/main/java/bomberOne/views/home/HomeViewImpl.java
@@ -0,0 +1,43 @@
+package bomberOne.views.home;
+
+import bomberOne.views.basic.ViewImpl;
+
+public class HomeViewImpl extends ViewImpl implements HomeView {
+
+    @Override
+    public void drawHomeView() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void render() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void switchToSetUp() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void switchToGame() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void switchToRank() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
