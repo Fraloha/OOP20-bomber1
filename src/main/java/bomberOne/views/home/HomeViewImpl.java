049d2b5d870945e658ccf21a9ea213b137d7f9cd
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2021-04-30 17:37:01 +0200

    Updated HomeView

--- src/main/java/bomberOne/views/home/HomeViewImpl.java
+++ src/main/java/bomberOne/views/home/HomeViewImpl.java
@@ -1,12 +1,24 @@
 package bomberOne.views.home;
 
+import bomberOne.controllers.game.GameController;
 import bomberOne.tools.img.GameImages;
+import bomberOne.views.ViewType;
+import bomberOne.views.ViewsSwitcher;
 import bomberOne.views.basic.ViewImpl;
+import bomberOne.views.game.movement.ControlsMap;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.ImageView;
 
 public class HomeViewImpl extends ViewImpl implements HomeView {
+
+    @FXML
+    private Canvas canvasBackground;
+
+    @FXML
+    private Canvas canvasForegrounds;
 
     @FXML
     private ImageView buttonPlay;
@@ -16,6 +28,10 @@ public class HomeViewImpl extends ViewImpl implements HomeView {
 
     @FXML
     private ImageView buttonTutorial;
+
+    private GraphicsContext gCForeground;
+    private GraphicsContext gCBackground;
+    private ControlsMap controlsMap;
 
     @Override
     public void drawHomeView() {
@@ -29,10 +45,9 @@ public class HomeViewImpl extends ViewImpl implements HomeView {
 
     }
 
-    @Override
+    @FXML
     public void switchToSetUp() {
-        // TODO Auto-generated method stub
-
+        ViewsSwitcher.switchView(this.getStage(), ViewType.GAME, this.getController().getModel());
     }
 
     @Override
@@ -49,30 +64,35 @@ public class HomeViewImpl extends ViewImpl implements HomeView {
 
     @Override
     public void init() {
-        // TODO Auto-generated method stub
 
     }
 
+    @FXML
     public void setPlay() {
         this.buttonPlay.setImage(SwingFXUtils.toFXImage(GameImages.PLAY_SET.getImage(), null));
     }
 
+    @FXML
     public void unsetPlay() {
         this.buttonPlay.setImage(SwingFXUtils.toFXImage(GameImages.PLAY_UNSET.getImage(), null));
     }
 
+    @FXML
     public void setRank() {
         this.buttonRank.setImage(SwingFXUtils.toFXImage(GameImages.RANK_SET.getImage(), null));
     }
 
+    @FXML
     public void unsetRank() {
         this.buttonRank.setImage(SwingFXUtils.toFXImage(GameImages.RANK_UNSET.getImage(), null));
     }
 
+    @FXML
     public void setTutorial() {
         this.buttonTutorial.setImage(SwingFXUtils.toFXImage(GameImages.TUTORIAL_SET.getImage(), null));
     }
 
+    @FXML
     public void unsetTutorial() {
         this.buttonTutorial.setImage(SwingFXUtils.toFXImage(GameImages.TUTORIAL_UNSET.getImage(), null));
     }
