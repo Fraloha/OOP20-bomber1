026ce28b4e220b2fd3cb744e3784185a79ed38ff
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2021-05-03 12:28:13 +0200

    Updated Home Buttons

--- src/main/java/bomberOne/BomberOne.java
+++ src/main/java/bomberOne/BomberOne.java
@@ -33,7 +33,7 @@ public final class BomberOne extends Application {
         test.getUser().setControls(Controls.ARROW);
         test.getUser().setSkin(Skins.WHITE);
         test.setDifficulty(Difficulty.STANDARD);
-//        test.init();
+//      test.init();
         ViewsSwitcher.switchView(primaryStage, ViewType.HOME, test);
     }
 
