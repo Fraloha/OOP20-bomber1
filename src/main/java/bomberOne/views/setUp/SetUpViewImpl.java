931e216b97c20684a349183e0f46102051747d7f
Tommaso <tommaso.brini@studio.unibo.it>
2021-05-03 13:01:24 +0200

    Create SetUpView

---
+++ src/main/java/bomberOne/views/setUp/SetUpViewImpl.java
@@ -0,0 +1,56 @@
+package bomberOne.views.setUp;
+
+import bomberOne.controllers.Controller;
+import javafx.stage.Stage;
+
+public class SetUpViewImpl implements SetUpView {
+
+    @Override
+    public void attachController(Controller controller) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Controller getController() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setStage(Stage stage) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Stage getStage() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setUp() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void switchToGame() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void switchToHome() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
