fe497f09d5e5b7402ccc6d8a97e268e7c5836ce7
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2021-05-12 17:31:17 +0200

    Refactoring HomeView

--- src/main/java/bomberone/views/home/HomeViewImpl.java
+++ src/main/java/bomberone/views/home/HomeViewImpl.java
@@ -1,12 +1,9 @@
 package bomberone.views.home;
 
 import bomberone.model.common.GameImages;
-import bomberone.tools.audio.SoundsHandler;
-import bomberone.tools.audio.GameSounds;
 import bomberone.views.ViewType;
 import bomberone.views.ViewsSwitcher;
 import bomberone.views.basic.ViewImpl;
-import bomberone.views.game.movement.ControlsMap;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
@@ -29,70 +26,89 @@ public class HomeViewImpl extends ViewImpl implements HomeView {
     @FXML
     private ImageView buttonRules;
 
-    // private GraphicsContext graphicContext;
-    private ControlsMap controlsMap;
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void init() {
-        // this.graphicContext = this.homeCanvas.getGraphicsContext2D();
-
-        this.drawHome();
         this.getController().init();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void drawHome() {
-        this.boxLogo.setImage(SwingFXUtils.toFXImage(GameImages.HOME_LOGO.getImage(), null));
-        this.buttonPlay.setImage(SwingFXUtils.toFXImage(GameImages.PLAY_UNSET.getImage(), null));
-        this.buttonRank.setImage(SwingFXUtils.toFXImage(GameImages.RANK_UNSET.getImage(), null));
-        this.buttonRules.setImage(SwingFXUtils.toFXImage(GameImages.RULES_UNSET.getImage(), null));
-    }
-
-    @FXML
     public void switchToSetUp() {
         ViewsSwitcher.switchView(this.getStage(), ViewType.SETUP, this.getController().getModel());
     }
 
-    @FXML
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void switchToCredits() {
         ViewsSwitcher.switchView(this.getStage(), ViewType.CREDITS, this.getController().getModel());
     }
 
-    @FXML
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void switchToRank() {
         ViewsSwitcher.switchView(this.getStage(), ViewType.RANK, this.getController().getModel());
     }
 
-    @FXML
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void switchToRules() {
         ViewsSwitcher.switchView(this.getStage(), ViewType.RULES, this.getController().getModel());
     }
 
+    /**
+     * Method that select button play.
+     */
     @FXML
     public void setPlay() {
         this.buttonPlay.setImage(SwingFXUtils.toFXImage(GameImages.PLAY_SET.getImage(), null));
     }
 
+    /**
+     * Method that deselect button play.
+     */
     @FXML
     public void unsetPlay() {
         this.buttonPlay.setImage(SwingFXUtils.toFXImage(GameImages.PLAY_UNSET.getImage(), null));
     }
 
+    /**
+     * Method that select button rank.
+     */
     @FXML
     public void setRank() {
         this.buttonRank.setImage(SwingFXUtils.toFXImage(GameImages.RANK_SET.getImage(), null));
     }
 
+    /**
+     * Method that deselect button rank.
+     */
     @FXML
     public void unsetRank() {
         this.buttonRank.setImage(SwingFXUtils.toFXImage(GameImages.RANK_UNSET.getImage(), null));
     }
 
+    /**
+     * Method that select button rules.
+     */
     @FXML
     public void setRules() {
         this.buttonRules.setImage(SwingFXUtils.toFXImage(GameImages.RULES_SET.getImage(), null));
     }
 
+    /**
+     * Method that deselect button rules.
+     */
     @FXML
     public void unsetRules() {
         this.buttonRules.setImage(SwingFXUtils.toFXImage(GameImages.RULES_UNSET.getImage(), null));
