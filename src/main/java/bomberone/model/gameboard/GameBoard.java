11b025dc12039e0e3a623902369c1d170cf1ef48
Weebli <infofrancesco20@gmail.com>
2021-05-25 23:09:38 +0200

    Created a GameBoard object to keep track of the map status.

--- src/main/java/bomberone/model/gameboard/GameBoard.java
+++ src/main/java/bomberone/model/gameboard/GameBoard.java
@@ -1,15 +1,22 @@
 package bomberone.model.gameboard;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import bomberone.model.common.P2d;
+
 public class GameBoard {
 
     /* Fields. */
+    private static final int WORLD_SIZE = 18;
     private char[][] currentGameBoard;
+    private char[][] markedBoard;
     private int rows;
     private int columns;
 
     /* Singleton pattern. */
     private static class GameBoardHolder {
-        private static final GameBoard BOARDSINGLETON = new GameBoard(18, 18);
+        private static final GameBoard BOARDSINGLETON = new GameBoard(GameBoard.WORLD_SIZE, GameBoard.WORLD_SIZE);
     }
 
     public static GameBoard getInstance() {
@@ -17,10 +24,11 @@ public class GameBoard {
     }
 
     /* Constructors. */
-    private GameBoard(int rowsToSet, int columnsToSet) {
+    private GameBoard(final int rowsToSet, final int columnsToSet) {
         this.rows = rowsToSet;
         this.columns = columnsToSet;
         this.currentGameBoard = new char[this.rows][this.columns];
+        this.markedBoard = new char[this.rows][this.columns];
     }
 
     /* Methods. */
@@ -59,15 +67,15 @@ public class GameBoard {
         }
     }
 
-    private boolean isLegal(int row, int column) {
+    private boolean isLegal(final int row, final int column) {
         return ((row >= 0 && row < 18) && (column >= 0 && column < 18));
     }
 
-    public void setItem(int row, int column, Markers marker) {
+    public void setItem(final int row, final int column, final Markers marker) {
         this.currentGameBoard[row][column] = this.isLegal(row, column) ? marker.getMarker() : 'N';
     }
 
-    public char getItem(int row, int column) {
+    public char getItem(final int row, final int column) {
         if (this.isLegal(row, column)) {
             return this.currentGameBoard[row][column];
         } else {
@@ -75,7 +83,7 @@ public class GameBoard {
         }
     }
 
-    public int[] findEnemyLocation(int enemyNumber) {
+    public int[] findEnemyLocation(final int enemyNumber) {
         int currentEnemy = 1;
 
         for (int i = 0; i < this.rows; i++) {
@@ -91,19 +99,105 @@ public class GameBoard {
         return new int[] { -1, -1 };
     }
 
-    public boolean isAccessible(int row, int column) {
-        return this.getItem(row, column) != 'W' && this.getItem(row, column) != 'B' ? true : false;
+    public boolean isAccessible(final int row, final int column) {
+        return this.getItem(row, column) != Markers.WALL_MARKER.getMarker()
+                && this.getItem(row, column) != Markers.BOX_MARKER.getMarker();
+    }
+
+    private boolean isRowAccessible(final int row, final int column, final int[] playerLocation) {
+        boolean result = true;
+        int i = Math.min(row, playerLocation[0]);
+        int m = Math.max(row, playerLocation[0]);
+
+        while (i < m) {
+            if (!this.isLegal(i, column) || !this.isAccessible(i, column)) {
+                result = false;
+                break;
+            }
+            i++;
+        }
+
+        return result;
+    }
+
+    private boolean isColumnAccessible(final int row, final int column, final int[] destination) {
+        boolean result = true;
+        int j = Math.min(column, destination[1]);
+        int m = Math.max(column, destination[1]);
+
+        while (j < m) {
+            if (!this.isLegal(row, j) || !this.isAccessible(row, j)) {
+                result = false;
+                break;
+            }
+            j++;
+        }
+
+        return result;
     }
 
     public boolean isPlayerVisible(int row, int column) {
         boolean result = false;
         int[] playerLocation = this.findPlayerLocation();
         if (this.isLegal(row, column) && this.isAccessible(row, column)) {
-            if (playerLocation[0] == row || playerLocation[1] == column) {
+            if (this.isRowAccessible(row, column, playerLocation)
+                    || this.isColumnAccessible(row, column, playerLocation)) {
                 result = true;
             }
         }
 
         return result;
+    }
+
+    public void setPlayerLocation(final int row, final int column) {
+        this.resetPlayerLocation();
+        this.setItem(row, column, Markers.PLAYER_MARKER);
+    }
+
+    public int[] convertLocation(final P2d location) {
+        int[] boardLocation = new int[2];
+
+        boardLocation[0] = (int) location.getX() / 32;
+        boardLocation[1] = (int) location.getY() / 32;
+
+        return boardLocation;
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.columns; j++) {
+                System.out.print(this.getItem(i, j) + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void setMarkedBoard() {
+        this.markedBoard = this.currentGameBoard;
+    }
+
+    public void printMarkedBoard() {
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.columns; j++) {
+                System.out.print(this.markedBoard[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void setPlaceHolder(int row, int column) {
+        if (this.isLegal(row, column)) {
+            this.markedBoard[row][column] = 'X';
+        }
+    }
+
+    public void resetPlaceHolder() {
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.columns; j++) {
+                if (this.currentGameBoard[i][j] == 'X') {
+                    this.currentGameBoard[i][j] = 'G';
+                }
+            }
+        }
     }
 }
