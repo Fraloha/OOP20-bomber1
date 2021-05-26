16a40d2543031b1089c6c08e350e84a4a9c8ff23
Weebli <infofrancesco20@gmail.com>
2021-05-26 23:24:36 +0200

    Some methods refactored.

--- src/main/java/bomberone/model/gameboard/GameBoard.java
+++ src/main/java/bomberone/model/gameboard/GameBoard.java
@@ -1,16 +1,10 @@
 package bomberone.model.gameboard;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import bomberone.model.common.P2d;
-
 public class GameBoard {
 
     /* Fields. */
     private static final int WORLD_SIZE = 18;
     private char[][] currentGameBoard;
-    private char[][] markedBoard;
     private int rows;
     private int columns;
 
@@ -28,7 +22,6 @@ public class GameBoard {
         this.rows = rowsToSet;
         this.columns = columnsToSet;
         this.currentGameBoard = new char[this.rows][this.columns];
-        this.markedBoard = new char[this.rows][this.columns];
     }
 
     /* Methods. */
@@ -48,100 +41,116 @@ public class GameBoard {
         return this.columns;
     }
 
-    public int[] findPlayerLocation() {
+    public BoardPoint findPlayerLocation() {
 
+        BoardPoint location = new BoardPointImpl(-1, -1);
         for (int i = 0; i < this.rows; i++) {
             for (int j = 0; j < this.columns; j++) {
-                if (this.getItem(i, j) == 'P') {
-                    return new int[] { i, j };
+                location.setPoint(i, j);
+                if (this.getItem(location) == 'P') {
+                    return location;
                 }
             }
         }
-        return new int[] { -1, -1 };
+        return new BoardPointImpl(-1, -1);
     }
 
     public void resetPlayerLocation() {
-        int[] location = this.findPlayerLocation();
-        if (location[0] != -1) {
-            this.currentGameBoard[location[0]][location[1]] = 'G';
+        BoardPoint location = this.findPlayerLocation();
+        if (location.getX() != -1) {
+            this.currentGameBoard[location.getX()][location.getY()] = 'G';
         }
     }
 
-    private boolean isLegal(final int row, final int column) {
-        return ((row >= 0 && row < 18) && (column >= 0 && column < 18));
+    private boolean isLegal(final BoardPoint locationToCheck) {
+        boolean result = locationToCheck.getX() >= 0 && locationToCheck.getX() < 18 ? true : false;
+        boolean secondResult = locationToCheck.getY() >= 0 && locationToCheck.getY() < 18 ? true : false;
+
+        return result && secondResult;
     }
 
-    public void setItem(final int row, final int column, final Markers marker) {
-        this.currentGameBoard[row][column] = this.isLegal(row, column) ? marker.getMarker() : 'N';
+    public void setItem(final BoardPoint locationToSet, final Markers marker) {
+        int currentX = locationToSet.getX();
+        int currentY = locationToSet.getY();
+        this.currentGameBoard[currentX][currentY] = this.isLegal(locationToSet) ? marker.getMarker() : 'N';
     }
 
-    public char getItem(final int row, final int column) {
-        if (this.isLegal(row, column)) {
-            return this.currentGameBoard[row][column];
+    public char getItem(final BoardPoint location) {
+        if (this.isLegal(location)) {
+            return this.currentGameBoard[location.getX()][location.getY()];
         } else {
             return 'N';
         }
     }
 
-    public int[] findEnemyLocation(final int enemyNumber) {
-        int currentEnemy = 1;
+    /*
+     * public int[] findEnemyLocation(final int enemyNumber) { int currentEnemy = 1;
+     * 
+     * for (int i = 0; i < this.rows; i++) { for (int j = 0; j < this.columns; j++)
+     * { if (this.getItem(i, j) == 'E' && currentEnemy == enemyNumber) { return new
+     * int[] { i, j }; } else { currentEnemy++; } } }
+     * 
+     * return new int[] { -1, -1 }; }
+     */
 
-        for (int i = 0; i < this.rows; i++) {
-            for (int j = 0; j < this.columns; j++) {
-                if (this.getItem(i, j) == 'E' && currentEnemy == enemyNumber) {
-                    return new int[] { i, j };
-                } else {
-                    currentEnemy++;
+    public boolean isAccessible(final BoardPoint locationToCheck) {
+        return this.getItem(locationToCheck) != Markers.WALL_MARKER.getMarker()
+                && this.getItem(locationToCheck) != Markers.BOX_MARKER.getMarker();
+    }
+
+    private boolean isRowAccessible(final BoardPoint currentPoint, final BoardPoint playerPosition) {
+
+        boolean result = true;
+
+        if (currentPoint.getY() == playerPosition.getY()) {
+            result = false;
+        } else {
+            BoardPoint nextPoint = new BoardPointImpl(0, 0);
+            int i = Math.min(currentPoint.getX(), playerPosition.getX());
+            int m = Math.max(currentPoint.getX(), playerPosition.getX());
+
+            while (i <= m) {
+                nextPoint.setPoint(i, m);
+                if (!this.isLegal(nextPoint) || !this.isAccessible(nextPoint)) {
+                    result = false;
+                    break;
                 }
+                i++;
             }
-        }
-
-        return new int[] { -1, -1 };
-    }
-
-    public boolean isAccessible(final int row, final int column) {
-        return this.getItem(row, column) != Markers.WALL_MARKER.getMarker()
-                && this.getItem(row, column) != Markers.BOX_MARKER.getMarker();
-    }
-
-    private boolean isRowAccessible(final int row, final int column, final int[] playerLocation) {
-        boolean result = true;
-        int i = Math.min(row, playerLocation[0]);
-        int m = Math.max(row, playerLocation[0]);
-
-        while (i < m) {
-            if (!this.isLegal(i, column) || !this.isAccessible(i, column)) {
-                result = false;
-                break;
-            }
-            i++;
         }
 
         return result;
     }
 
-    private boolean isColumnAccessible(final int row, final int column, final int[] destination) {
+    private boolean isColumnAccessible(final BoardPoint currentPoint, final BoardPoint playerPosition) {
         boolean result = true;
-        int j = Math.min(column, destination[1]);
-        int m = Math.max(column, destination[1]);
-
-        while (j < m) {
-            if (!this.isLegal(row, j) || !this.isAccessible(row, j)) {
-                result = false;
-                break;
+        
+        if (currentPoint.getX() == playerPosition.getX()) {
+            result = false;
+        } else {
+            BoardPoint nextPoint = new BoardPointImpl(0, 0);
+            int j = Math.min(currentPoint.getY(), playerPosition.getY());
+            int m = Math.max(currentPoint.getY(), playerPosition.getY());
+            
+            while (j <= m) {
+                nextPoint.setPoint(j, m);
+                if (!this.isLegal(nextPoint) || !this.isAccessible(nextPoint)) {
+                    result = false;
+                    break;
+                }
+                j++;
             }
-            j++;
         }
-
+        
         return result;
     }
 
-    public boolean isPlayerVisible(int row, int column) {
+    public boolean isPlayerVisible(BoardPoint currentPosition) {
         boolean result = false;
-        int[] playerLocation = this.findPlayerLocation();
-        if (this.isLegal(row, column) && this.isAccessible(row, column)) {
-            if (this.isRowAccessible(row, column, playerLocation)
-                    || this.isColumnAccessible(row, column, playerLocation)) {
+        BoardPoint playerLocation = this.findPlayerLocation();
+        if (this.isLegal(currentPosition) && this.isAccessible(currentPosition)) {
+            if (this.isRowAccessible(currentPosition, playerLocation)
+                    || this.isColumnAccessible(currentPosition, playerLocation)) {
                 result = true;
             }
         }
@@ -149,55 +158,17 @@ public class GameBoard {
         return result;
     }
 
-    public void setPlayerLocation(final int row, final int column) {
+    public void setPlayerLocation(final BoardPoint newPosition) {
         this.resetPlayerLocation();
-        this.setItem(row, column, Markers.PLAYER_MARKER);
-    }
-
-    public int[] convertLocation(final P2d location) {
-        int[] boardLocation = new int[2];
-
-        boardLocation[0] = (int) location.getX() / 32;
-        boardLocation[1] = (int) location.getY() / 32;
-
-        return boardLocation;
+        this.setItem(newPosition, Markers.PLAYER_MARKER);
     }
 
     public void printBoard() {
         for (int i = 0; i < this.rows; i++) {
             for (int j = 0; j < this.columns; j++) {
-                System.out.print(this.getItem(i, j) + "  ");
+                System.out.print(this.getItem(new BoardPointImpl(i, j)) + "  ");
             }
             System.out.println();
-        }
-    }
-
-    public void setMarkedBoard() {
-        this.markedBoard = this.currentGameBoard;
-    }
-
-    public void printMarkedBoard() {
-        for (int i = 0; i < this.rows; i++) {
-            for (int j = 0; j < this.columns; j++) {
-                System.out.print(this.markedBoard[i][j] + "  ");
-            }
-            System.out.println();
-        }
-    }
-
-    public void setPlaceHolder(int row, int column) {
-        if (this.isLegal(row, column)) {
-            this.markedBoard[row][column] = 'X';
-        }
-    }
-
-    public void resetPlaceHolder() {
-        for (int i = 0; i < this.rows; i++) {
-            for (int j = 0; j < this.columns; j++) {
-                if (this.currentGameBoard[i][j] == 'X') {
-                    this.currentGameBoard[i][j] = 'G';
-                }
-            }
         }
     }
 }
