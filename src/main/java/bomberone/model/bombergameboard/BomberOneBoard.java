544fcbc041c3eb6b89762b570005706bc7a8566d
Weebli <infofrancesco20@gmail.com>
2021-06-10 10:22:26 +0200

    Created a package that contains a object that is the map of this game.

---
+++ src/main/java/bomberone/model/bombergameboard/BomberOneBoard.java
@@ -0,0 +1,28 @@
+package bomberone.model.bombergameboard;
+
+import bomberone.model.common.P2d;
+import bomberone.model.common.Maps;
+import bomberone.model.gameboard.GameBoard;
+import bomberone.model.gameboard.BoardPoint;
+import bomberone.model.gameboard.BoardPointImpl;
+
+public final class BomberOneBoard extends GameBoard{
+    
+    private static final int CELL_SIZE = 32;
+    private static final BomberOneBoard SINGLETON = new BomberOneBoard();
+    
+    private BomberOneBoard() {
+        super(Maps.MAP1);
+    }
+    
+    public static BomberOneBoard getInstance() {
+        return BomberOneBoard.SINGLETON;
+    }
+    
+    public BoardPoint convertPosition(P2d positionToConvert) {
+        int X = (int) Math.round(positionToConvert.getY() / BomberOneBoard.CELL_SIZE);
+        int Y = (int) Math.round(positionToConvert.getX() / BomberOneBoard.CELL_SIZE);
+        
+        return new BoardPointImpl(X, Y);
+    }
+}
