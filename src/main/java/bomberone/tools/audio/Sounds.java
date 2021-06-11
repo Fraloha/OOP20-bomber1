b9480d9a26554055f1e9d0877748b53b341f1fe4
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2021-06-11 11:17:39 +0200

    JavaDoc Sounds

--- src/main/java/bomberone/tools/audio/Sounds.java
+++ src/main/java/bomberone/tools/audio/Sounds.java
@@ -3,12 +3,12 @@ package bomberone.tools.audio;
 public enum Sounds {
 
     /**
-     * 
+     * This type represents the sound effects of the game.
      */
     EFFECT,
 
     /**
-     * 
+     * This type represents the audio backgrounds of the game.
      */
     AUDIO;
 
