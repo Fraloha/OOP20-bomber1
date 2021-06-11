4515f4b72b879a594c7332f95021a80b01733ddb
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2021-06-11 17:43:42 +0200

    JavaDoc HomeController

--- src/main/java/bomberone/controllers/home/HomeControllerImpl.java
+++ src/main/java/bomberone/controllers/home/HomeControllerImpl.java
@@ -5,6 +5,7 @@ import bomberone.controllers.ControllerImpl;
 public class HomeControllerImpl extends ControllerImpl implements HomeController {
 
     /**
+     * Generic controller for home views that do not require specific features.
      */
     @Override
     public void init() {
